f1ce1a0853731c4bcaa40c5fcb2f3ac27c6f0732
Pranith Madishetti <pranithsai.347@gmail.com>
2021-06-17 20:46:39 +0530

    Quiz_App day_2

---
+++ app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.quizapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var q_btn = findViewById<Button>(R.id.quiz_btn)
+        var q_name = findViewById<EditText>(R.id.quiz_name)
+
+        q_btn.setOnClickListener(View.OnClickListener {
+            Log.i("---------","Hello")
+            if(q_name.text.toString().isEmpty()){
+                Toast.makeText(this,"Please enter your name",Toast.LENGTH_LONG).show()
+            }
+            else{
+                var next:Intent = Intent(this,QuizActivity()::class.java)
+                startActivity(next)
+                finish()
+            }
+        })
+    }
+}
