b1d1957f8f387d8c200492bcdf428bee97415420
Pranith Madishetti <pranithsai.347@gmail.com>
2021-06-18 10:01:54 +0530

    Quiz_App day_2_p1

--- app/src/main/java/com/example/quizapp/Utility.kt
+++ app/src/main/java/com/example/quizapp/Utility.kt
@@ -7,6 +7,8 @@ class Utility {
         //logic
         var q1 = Questions(1,"What is kotlin?","Scripting Language","Programming language","Modern Language", "Assembly Language")
         var q2 = Questions(1,"What is Java?","Scripting Language","Programming language","Modern Language", "Assembly Language")
+        var q3 = Questions(1,"What is C++?","Scripting Language","Programming language","Modern Language", "Assembly Language")
+
         list.add(q1);
         list.add(q2);
 
